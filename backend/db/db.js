3c3b8a805b30d7fb928e471d06f48c1767ea28fc
aarya2141717 <aaryathapa870@example.com>
2025-12-02 16:58:57 +0545

    feat: setup backend and env files

---
+++ backend/db/db.js
@@ -0,0 +1,27 @@
+// backend/db/db.js
+const { Sequelize } = require("sequelize");
+require("dotenv").config({ path: __dirname + '/../.env' });
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected");
+  } catch (err) {
+    console.error("❌ Database connection error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDB };
