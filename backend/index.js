2234265a3a4c25980488d485a9a925ec07690ae1
aarya2141717 <aaryathapa870@example.com>
2025-12-02 16:58:57 +0545

    feat: setup backend and env files

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+// backend/index.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: __dirname + '/.env' });
+
+const { connectDB } = require("./db/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("EventEase API is running"));
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
