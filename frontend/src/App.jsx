87011ab7e36fa9e0d3dca663a2ffb2846b1b671e
pranishprajapati <pranishprajapati07@gmail.com>
2025-12-27 23:37:45 +0545

    Update artist and artistDetails data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
-
-// Import ForgotPassword when you create it
+import VenueDetails from './pages/VenueDetails';
+import ArtistDetails from './pages/ArtistDetails';
+import Venue from './pages/Venue';
+import Artists from './pages/Artists';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-
-        {/* Add when you create ForgotPassword */}
-        {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        <Route path="/venue/:venueId" element={<VenueDetails />} />
+        <Route path="/artist/:artistId" element={<ArtistDetails />} />
+        <Route path="/venue" element={<Venue />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/venue/:venueId" element={<VenueDetails />} />
+        <Route path="/artist/:artistId" element={<ArtistDetails />} />
       </Routes>
     </Router>
   );
