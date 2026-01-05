7dca59dbbaf6cea141cbaf0e66883e9159b18155
pranishprajapati <pranishprajapati07@gmail.com>
2026-01-05 12:22:07 +0545

    Updated all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,10 +9,12 @@ import VenueDetails from './pages/VenueDetails';
 import ArtistDetails from './pages/ArtistDetails';
 import Venue from './pages/Venue';
 import Artists from './pages/Artists';
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -24,6 +27,8 @@ export default function App() {
         <Route path="/venue/:venueId" element={<VenueDetails />} />
         <Route path="/artist/:artistId" element={<ArtistDetails />} />
       </Routes>
-    </Router>
+      <Footer />
+    </BrowserRouter>
+    
   );
 }
